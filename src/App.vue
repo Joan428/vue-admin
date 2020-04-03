85bd4ee66e44b24cab38dac652e2199b34952155
kate <15674110487@163.com>
2020-04-03 23:45:30 +0800

    构建项目、配置全局样式、路由、elementUI

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Login></Login>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Login from 'views/login/Login'
+export default {
+  name: 'App',
+  components: {
+    Login,
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/css/main.scss';
+
+
+</style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrap">
+    <el-button>默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"",
+  data(){
+    return {
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
